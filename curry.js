7f9372de9e06391b7280f0a751232b2430739074
YanPing Liu <yanping.liu@questmobile.com.cn>
2019-07-30 16:38:52 +0800

    更新去重函数

--- curry.js
+++ curry.js
@@ -1,35 +1,53 @@
-//简易版
-const curry = function (fn,args) {
+//需求：
+// var fn = curry(function (a, b, c) {
+//     console.log([a, b, c]);
+// });
+
+// fn("a", "b", "c") // ["a", "b", "c"]
+// fn("a", "b")("c") // ["a", "b", "c"]
+// fn("a")("b")("c") // ["a", "b", "c"]
+// fn("a")("b", "c") // ["a", "b", "c"]
+
+
+const curry = function (fn, args) {
     let lens = fn.length //参数长度
-    let args = args ||[]
+    let args = args || []
     function _curry() {
         let lastArgs = args//已经调用的参数
         for (let i = 0; i < arguments.length; i++) {//遍历此次传入的参数
             arg = arguments[i]
             lastArgs.push(arg)
         }
-        if(lastArgs.length < lens){//如果没达到调用参数长度，继续调用
+        if (lastArgs.length < lens) {//如果没达到调用参数长度，继续调用
             return curry.call(this, fn, lastArgs);
-        }else{//达到了则直接返回方法
+        } else {//达到了则直接返回方法
             return fn.apply(this, lastArgs);
         }
     }
     return _curry
 }
+//优雅的写法
+let curry = fn =>
+    judge = (...args) =>
+        args.length === fn.length
+            ? fn(...args)
+            : (arg) => judge(...args, arg)
 
 
 
-var fn = curry(function (a, b, c) {
-    console.log([a, b, c]);
-});
-
-fn("a", "b", "c") // ["a", "b", "c"]
-fn("a", "b")("c") // ["a", "b", "c"]
-fn("a")("b")("c") // ["a", "b", "c"]
-fn("a")("b", "c") // ["a", "b", "c"]
 
 
-//占位符版本
+//需求：
+//var fn = curry(function(a, b, c, d, e) {
+//     console.log([a, b, c, d, e]);
+// });
+// fn(1, 2, 3, 4, 5);
+// fn(_, 2, 3, 4, 5)(1);
+// fn(1, _, 3, 4, 5)(2);
+// fn(1, _, 3)(_, 4)(2)(5);
+// fn(1, _, _, 4)(_, 3)(2)(5);
+// fn(_, 2)(_, _, 4)(1)(3)(5)
+// 输出全部都是 [1, 2, 3, 4, 5]
 function curry(fn, args, holes) {
     length = fn.length;//参数长度
 
@@ -39,8 +57,8 @@ function curry(fn, args, holes) {
 
     return function () {
 
-        var _args = args.slice(0),
-            _holes = holes.slice(0),
+        let _args = args,
+            _holes = holes,
             argsLen = args.length,
             holesLen = holes.length,
             arg, i, index = 0;
@@ -85,13 +103,3 @@ function curry(fn, args, holes) {
         }
     }
 }
-var fn = curry(function(a, b, c, d, e) {
-    console.log([a, b, c, d, e]);
-});
-// 验证 输出全部都是 [1, 2, 3, 4, 5]
-fn(1, 2, 3, 4, 5);
-fn(_, 2, 3, 4, 5)(1);
-fn(1, _, 3, 4, 5)(2);
-fn(1, _, 3)(_, 4)(2)(5);
-fn(1, _, _, 4)(_, 3)(2)(5);
-fn(_, 2)(_, _, 4)(1)(3)(5)
--- unique.js
+++ unique.js
@@ -1,2 +1,28 @@
 //需求：数组内容去重
+//
+let unique = arr => [...new Set(arr)]
+let unique = arr => {
+    const map = new Map()
+    return arr.filter(item => !map.has(item) && map.set(item, 1))
+}
+//复杂类型
+var array = [{value: 1}, {value: 1}, {value: 2}];
 
+function unique(array) {
+    var obj = {};
+    return array.filter(function(item){
+        console.log(typeof item + JSON.stringify(item))
+        return obj.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true)
+    })
+}
+
+console.log(unique(array)); // [{value: 1}, {value: 2}]
+
+let array = [1,2,3,4,1]
+let unique = function(){
+    let arr = [...array]
+    arr.filter((item,index,array)=>{
+        return array.indexOf(item) === index
+    })
+    return arr
+}
